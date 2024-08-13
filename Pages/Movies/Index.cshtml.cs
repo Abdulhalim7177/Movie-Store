c08b4697a9850387ea2ce431ce4b2abd630f192c
Abdulhalim Muhammad Yaro <123118801+Abdulhalim7177@users.noreply.github.com>
2024-08-13 17:15:23 +0100

    update

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieStore.Data;
+using MovieStore.Models;
+
+namespace MovieStore.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieStore.Data.MovieStoreContext _context;
+
+        public IndexModel(MovieStore.Data.MovieStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
